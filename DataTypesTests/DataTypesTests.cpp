07989efe37007f969e7d67ba108c3839756f849b
James Mills <james.mills@aie.edu.au>
2026-02-26 11:49:19 +1100

    Initial commit

---
+++ DataTypesTests/DataTypesTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace DataTypesTests
+{
+	TEST_CLASS(DataTypesTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
